6f8733c0d12ffb4362503ccf7133bf4664797baf
anvaka <anvaka@gmail.com>
2014-05-15 23:24:41 -0700

    Draw sin function

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,21 +3,34 @@ document.body.onload = main;
 function main() {
   var canvas = document.getElementById('scene');
   var ctx = canvas.getContext('2d');
-  
+
   var plot = ctx.getImageData(0, 0, canvas.offsetWidth, canvas.offsetHeight);
-  
+
   var width = plot.width;
   var height = plot.height;
-  for (var x = 0; x < width; ++x) {
-    for (var y = 0; y < height; ++y) {
-      pixel(x, y,  0, (x + y) % 255, 0);
-    }
+
+  var fromX = -10, toX = 10, fromY = -10, toY = 10;
+  var mathWidth = toX - fromX;
+  var mathHeight = toY - fromY;
+
+  var step = (toX - fromX)/(20 * width);
+
+  for (var x = fromX; x < toX; x += step) {
+    var y = Math.sin(x);
+
+    var screenX = (x - fromX) * width / mathWidth;
+    var screenY = (y - fromY) * height / mathHeight;
+
+    pixel(screenX, screenY, 0, 255, 0);
   }
+
   ctx.putImageData(plot, 0, 0);
-  
+
   function pixel(x, y, r, g, b) {
+    x = Math.floor(x);
+    y = Math.floor(y);
     var idx = (x + width * y) * 4;
-    plot.data[idx] = r; 
+    plot.data[idx] = r;
     plot.data[idx + 1] = g;
     plot.data[idx + 2] = b;
     plot.data[idx + 3] = 255;
