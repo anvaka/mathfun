78b4e6c6561d9c6ce6df52941405f3b5e00ab6dd
anvaka <anvaka@gmail.com>
2014-05-18 12:26:13 -0700

    Added heightmap

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,44 +1,23 @@
+// based on: http://www.playfuljs.com/realistic-terrain-in-130-lines/
+
 document.body.onload = main;
 
 function main() {
   var canvas = document.getElementById('scene');
   var ctx = canvas.getContext('2d');
-
   var plot = ctx.getImageData(0, 0, canvas.offsetWidth, canvas.offsetHeight);
-
+  var map = heightMap(9, 0.7);
   var width = plot.width;
   var height = plot.height;
 
-  // play with this:
-  var fromX = -10, toX = 10, fromY = -10, toY = 10;
-  var mathWidth = toX - fromX;
-  var mathHeight = toY - fromY;
-
-  var step = (toX - fromX)/(20 * width);
-  var t = 0;
-
-  frame();
-
-  function frame() {
-    //requestAnimationFrame(frame);
-    ctx.clearRect(0, 0, width, height);
-    plot = ctx.getImageData(0, 0, width, height);
-    for (var x = fromX; x < toX; x += step) {
-      //  2. use t to change x; Play with y = x * sin(x);
-      var y = Math.sin(x);
-
-      var screenX = (x - fromX) * width / mathWidth;
-      var screenY = (y - fromY) * height / mathHeight;
-
-      // 0. Play with random
-      // 1. add t to move to the left
-      // 3. use t to animate color
-      pixel(screenX, screenY, 0, 255, 0);
-    // pixel(screenX + 5 - Math.random() * 10 , screenY + 5 - Math.random() * 10, 0, 255, 0);
+  for (var x = 0; x < width; ++x) {
+    for (var y = 0; y < height; ++y) {
+      var c = map.get(x, y);
+//      pixel(x, y, 0, c/1.8,0);
     }
-    ctx.putImageData(plot, 0, 0);
   }
 
+  ctx.putImageData(plot, 0, 0);
 
   function pixel(x, y, r, g, b) {
     x = Math.floor(x);
@@ -49,4 +28,73 @@ function main() {
     plot.data[idx + 2] = b;
     plot.data[idx + 3] = 255;
   }
+}
+
+// show slide with pictures
+function heightMap(detail, roughness) {
+  var size = Math.pow(2, detail) + 1,
+      max = size - 1,
+      map = new Float32Array(size * size);
+
+  set(0, 0, max / 2);
+  set(max, 0, max / 2);
+  set(max, max, max / 2);
+  set(0, max, max / 2);
+
+  divide(max);
+
+  return {
+    map: map,
+    get: get
+  };
+
+  function divide(size) {
+    var x, y, half = size / 2;
+    var scale = roughness * size;
+    if (half < 1) return;
+
+    for (y = half; y < max; y += size) {
+      for (x = half; x < max; x += size) {
+        square(x, y, half, Math.random() * scale * 2 - scale);
+      }
+    }
+    for (y = 0; y <= max; y += half) {
+      for (x = (y + half) % size; x <= max; x += size) {
+        diamond(x, y, half, Math.random() * scale * 2 - scale);
+      }
+    }
+    divide(size / 2);
+  }
+
+  function diamond(x, y, size, offset) {
+    var avg = average(
+      get(x, y - size),      // top
+      get(x + size, y),      // right
+      get(x, y + size),      // bottom
+      get(x - size, y)       // left
+    );
+
+    set(x, y, avg + offset);
+  }
+
+  function square(x, y, size, offset) {
+    var avg = average(
+      get(x - size, y - size),      // left, top
+      get(x + size, y - size),      // right, top
+      get(x + size, y + size),      // right, bottom
+      get(x - size, y + size)       // left, bottom
+    );
+    set(x, y, avg + offset);
+  }
+
+  function average(w, x, y, z) {
+    return (w + x + y + z) / 4;
+  }
+
+  function get(x, y) {
+    if (x < 0 || x > max || y < 0 || y > max) return -1;
+    return map[x + size * y];
+  }
+
+  function set(x, y, value) { map[x + size * y] = value; }
 }
